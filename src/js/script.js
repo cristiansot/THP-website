8199af9a1d9d4fc9f31582195b15d78ed9b113cb
cristiansot <soluciongrafix@gmail.com>
2024-01-16 15:09:30 -0300

    developing contact form

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,11 @@ $(".navTrigger").click(function () {
 });
 
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-        $('.nav').addClass('affix');
-        console.log("OK");
-    } else {
-        $('.nav').removeClass('affix');
-    }
+  if ($(document).scrollTop() > 50) {
+      $('.nav').addClass('affix');
+      console.log("OK");
+  } else {
+      $('.nav').removeClass('affix');
+  }
 });
+
